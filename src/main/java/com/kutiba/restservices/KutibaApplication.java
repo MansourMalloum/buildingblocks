95085729b592507868f41c7a91f66897a8b9ac9b
Mansour <mansourmalloum@gmail.com>
2021-08-15 16:49:44 +0100

    First commit:

---
+++ src/main/java/com/kutiba/restservices/KutibaApplication.java
@@ -0,0 +1,13 @@
+package com.kutiba.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KutibaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KutibaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kutiba/restservices/KutibaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kutiba.restservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KutibaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
